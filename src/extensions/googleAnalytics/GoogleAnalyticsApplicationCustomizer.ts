3549c90566f87ed35b4b30f06039189c7ac72de8
gabmor38 <gabriela.morenor@gmail.com>
2024-08-12 10:42:04 -0400

    manually tagged GA into head of site

--- src/extensions/googleAnalytics/GoogleAnalyticsApplicationCustomizer.ts
+++ src/extensions/googleAnalytics/GoogleAnalyticsApplicationCustomizer.ts
@@ -16,6 +16,7 @@ const LOG_SOURCE: string = 'GoogleAnalyticsApplicationCustomizer';
 export interface IGoogleAnalyticsApplicationCustomizerProperties {
   // This is an example; replace with your own property
   testMessage: string;
+  id: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
@@ -24,16 +25,39 @@ export default class GoogleAnalyticsApplicationCustomizer
 
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+    
+    this.context.application.navigatedEvent.add(this, this.addGoogleTag);
+ 
 
     let message: string = this.properties.testMessage;
+
     if (!message) {
       message = '(No properties were provided.)';
+
     }
 
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`).catch(() => {
+    Dialog.alert(`${this.properties.id}`).catch(() => {
       /* handle error */
     });
 
     return Promise.resolve();
   }
+
+  public addGoogleTag ():void {
+  
+    const scriptTag = document.createElement('script');
+    scriptTag.async = true;
+    scriptTag.src = `https://www.googletagmanager.com/gtag/js?id=${this.properties.id}`;
+    const scriptContent = document.createElement('script');
+    scriptContent.innerHTML = `
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+     gtag('config', '${this.properties.id}');
+    `
+   document.head.appendChild(scriptTag);
+   document.head.appendChild(scriptContent);
+
+  }
+
 }
